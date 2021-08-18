7b8a29c5efe902aed4ff7c6ade1b6a3eab5a9d2c
sunke <126680611@qq.com>
2021-08-19 02:18:38 +0800

    add a part of the code about security API test.

--- src/test/java/com/example/fundmanager/controller/SecurityControllerTests.java
+++ src/test/java/com/example/fundmanager/controller/SecurityControllerTests.java
@@ -4,10 +4,10 @@ import com.example.fundmanager.dao.FundManagerRepository;
 import com.example.fundmanager.dao.FundRepository;
 import com.example.fundmanager.dao.PositionRepository;
 import com.example.fundmanager.dao.SecurityRepository;
+import com.example.fundmanager.entity.Fund;
 import com.example.fundmanager.entity.Position;
 import com.example.fundmanager.entity.Security;
-import com.example.fundmanager.exception.PositionNotFoundException;
-import com.example.fundmanager.exception.SecurityNotFoundException;
+import com.example.fundmanager.exception.*;
 import com.example.fundmanager.service.FundManagerService;
 import com.example.fundmanager.service.FundService;
 import com.example.fundmanager.service.PositionService;
@@ -17,14 +17,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDate;
 import java.util.List;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -95,5 +99,113 @@ public class SecurityControllerTests {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/security/1")).andExpect(status().isNotFound());
     }
 
+    @Test
+    public void testAddSecuritySuccess() throws Exception{
+        String json = "{\n" +
+                "    \"symbol\": \"some symbol\",\n" +
+                "}";
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/api/security")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request).andExpect(status().isOk());
+
+        verify(securityService).addSecurity(any(Security.class));
+    }
+
+    @Test
+    public void testAddSecurityAlreadyInUse() throws Exception{
+        String json = "{\n" +
+                "    \"symbol\": \"IBM\",\n" +
+                "}";
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/api/security")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request).andExpect(status().isBadRequest());
+
+        verify(securityService).addSecurity(any(Security.class));
+    }
+
+    @Test
+    public void testUpdateSecuritySuccess() throws Exception{
+        String json = "{\n" +
+                "    \"securityId\": 1,\n" +
+                "    \"symbol\": \"Oracle\",\n" +
+                "}";
+        RequestBuilder request = MockMvcRequestBuilders
+                .put("/api/security/1")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON);
+        mockMvc.perform(request).andExpect(status().isOk());
+
+        verify(securityService).updateSecurity(anyLong(),any(Security.class));
+    }
+
+    @Test
+    public void testUpdateIdNotMatching() throws Exception{
+        doThrow(FundIdNotMatchingException.class).when(securityService).updateSecurity(anyLong(), any(Security.class));
+
+        String json = "{\n" +
+                "    \"securityId\": 1,\n" +
+                "    \"symbol\": \"Oracle\",\n" +
+                "}";
+        RequestBuilder request = MockMvcRequestBuilders
+                .put("/api/security/2")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request).andExpect(status().isBadRequest());
+        verify(securityService).updateSecurity(anyLong(), any(Security.class));
+    }
+
+    @Test
+    public void testUpdateAlreadyInUse() throws Exception{
+        doThrow(SecurityAlreadyInUseException.class).when(securityService).updateSecurity(anyLong(), any(Security.class));
+
+        String json = "{\n" +
+                "    \"securityId\": 1,\n" +
+                "    \"symbol\": \"IBM\",\n" +
+                "}";
+        RequestBuilder request = MockMvcRequestBuilders
+                .put("/api/fund/1")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request).andExpect(status().isBadRequest());
+        verify(securityService).updateSecurity(anyLong(), any(Security.class));
+    }
+
+    @Test
+    public void testUpdateIllegalUpdatedFund() throws Exception{
+        doThrow(IllegalUpdatedSecurityException.class).when(securityService).updateSecurity(anyLong(), any(Security.class));
+
+        String json = "{\n" +
+                "    \"securityId\": 1,\n" +
+                "    \"symbol\": null,\n" +
+                "}";
+        RequestBuilder request = MockMvcRequestBuilders
+                .put("/api/fund/1")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request).andExpect(status().isBadRequest());
+        verify(securityService).updateSecurity(anyLong(), any(Security.class));
+    }
+
+    @Test
+    public void testRemoveFundSuccess() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/security/1")).andExpect(status().isOk());
+        verify(securityService).removeSecurity(anyLong());
+    }
+
+    @Test
+    public void testRemoveNotFund() throws Exception{
+        doThrow(SecurityNotFoundException.class).when(securityService).removeSecurity(anyLong());
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/security/3")).andExpect(status().isNotFound());
+        verify(securityService).removeSecurity(anyLong());
+    }
 
 }
