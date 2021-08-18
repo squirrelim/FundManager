138a82782f315c241d28a50af40f82facb6fe92b
shoucai <sunshoucai1@gmail.com>
2021-08-18 17:40:59 +0800

    add the PositionControllerTests.java

---
+++ src/test/java/com/example/fundmanager/controller/PositionControllerTests.java
@@ -0,0 +1,10 @@
+package com.example.fundmanager.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PositionControllerTests {
+
+
+}
